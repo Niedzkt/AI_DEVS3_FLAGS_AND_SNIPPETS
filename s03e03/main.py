a09e6bc4638187341353d0312e3417e49ef21bcb
Niedzkt <136270909+Niedzkt@users.noreply.github.com>
2025-08-21 15:07:58 +0200

    s03e03 flaga i kod

--- s03e03/main.py
+++ s03e03/main.py
@@ -28,4 +28,51 @@ def send_to_centrala(task, answer):
     
     return response.text
 
+def send_query(query):
+    url = 'https://c3ntrala.ag3nts.org/apidb'
+    data = {
+            "task":"database",
+            "apikey":AIDEVS_API_KEY,
+            "query":query
+            }
+    response = requests.post(url, json=data)
 
+    return response
+
+def ask_llm(table_list, tables_dict, text):
+    response = openai.chat.completions.create(
+            model='gpt-4o',
+            messages=[
+                {'role':'system',
+                 'content':read_prompt('system_prompt.txt')},
+                {'role':'user',
+                 'content': f'Lista tabel: {table_list} oraz ich schematy: {tables_dict}. Na ich podstawie chcę uzyskać zapytanie sql które {text}'
+                 }
+                ]
+            )
+    answer = response.choices[0].message.content
+    return answer
+
+while True:
+    tables_dict = {}
+    response = send_query('show tables')
+    data = response.json()
+    table_list = [list(item.values())[0] for item in data['reply']]
+    print(table_list)
+    for value in table_list:
+        response = send_query(f'show create table {value}')
+        data = response.json()
+
+        row = data['reply'][0]
+        table_name = row['Table']
+        create_stmt = row['Create Table']
+        tables_dict[table_name] = create_stmt
+
+    text = input('Podaj zapytanie do systemu: ')
+    query = ask_llm(table_list, tables_dict, text)
+    answer = send_query(query)
+    print(f'Zapytanie do bazy: {query}\n\nOdpowiedź bazy: {answer.text}')
+    data = answer.json()
+    dc_table_list = [list(item.values())[0] for item in data['reply']]
+    print(f'Lista datacenter id: {dc_table_list}')
+    print(send_to_centrala('database',dc_table_list))
